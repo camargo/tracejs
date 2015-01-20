a9b0cd2e0313da6fc1958f91aacce8dd04fa7e42
camargo <camargo.cac@gmail.com>
2015-01-20 01:33:34 -0800

    made package.json, started Point3D

---
+++ src/Utilities/Point3D.js
@@ -0,0 +1,27 @@
+(function(){
+    'use strict';
+
+    var Point3D = function (x, y, z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    };
+
+    Point3D.prototype.x = function () {
+        return this.x;
+    };
+
+    Point3D.prototype.y = function () {
+        return this.y;
+    };
+
+    Point3D.prototype.z = function () {
+        return this.z;
+    };
+
+    Point3D.prototype.d_squared = function (point) {
+        return (this.x - point.x) * (this.x - point.x) +
+               (this.y - point.y) * (this.y - point.y) +
+               (this.z - point.z) * (this.z - point.z);
+    };
+}());
