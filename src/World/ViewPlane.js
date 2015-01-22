dd8ef87cbd7152176637fc26cf86e73193bc29dc
Shahar Zimmerman <szimmer1@ucsc.edu>
2015-01-21 23:17:36 -0800

    added ViewPlane class and unit tests. Couldn't get Karma working

---
+++ src/World/ViewPlane.js
@@ -0,0 +1,56 @@
+/*
+Shahar Zimmerman
+1/21/2015
+ */
+
+var Tracerjs = Tracerjs || {};
+
+(function() {
+
+    Tracerjs.ViewPlane = function(options) {
+        if (typeof options == 'object' && typeof options.hres != 'undefined') {
+            this.hres = options.hres;
+        }
+        else {
+            this.hres = 150;
+        }
+        if (typeof options == 'object' && typeof options.vres != 'undefined') {
+            this.vres = options.vres;
+        }
+        else {
+            this.vres = 300;
+        }
+        if (typeof options == 'object' && typeof options.psize != 'undefined') {
+            this.psize = options.psize;
+        }
+        else {
+            this.psize = 1;
+        }
+    }
+
+    Tracerjs.ViewPlane.prototype = {
+        /* getters and setters */
+        getHres : function() {
+            return this.hres
+        },
+        setHres : function(hres) {
+            this.hres = hres;
+            return this
+        },
+        getVres : function() {
+            return this.vres
+        },
+        setVres : function(vres) {
+            this.vres = vres;
+            return this
+        },
+        getPsize : function() {
+            return this.psize
+        },
+        setPsize : function(psize) {
+            this.psize = psize;
+            return this
+        }
+    }
+
+})();
---
+++ tests/ViewPlaneTest.js
@@ -0,0 +1,40 @@
+/**
+ * Created by mzimmerman on 1/21/15.
+ */
+
+describe("Is karma working", function() {
+    it("should work", function() {
+        expect(true).toEqual(true)
+    })
+});
+
+describe("ViewPlane class", function() {
+
+    it("should default to vres=150, hres=300, psize=1 when no arguments given", function() {
+
+        var viewplane = new Tracerjs.ViewPlane();
+
+        expect(viewplane.getHres()).toEqual(300);
+        expect(viewplane.getVres()).toEqual(150);
+        expect(viewplane.getPsize()).toEqual(1);
+    });
+
+    it("should take on correct hres and vres when specified in arguments", function() {
+
+        var hres = 100;
+        var vres = 50;
+        var psize = 5;
+
+        var viewPlane = new Tracerjs.ViewPlane({
+            hres : hres,
+            vres : vres,
+            psize : psize
+        });
+
+        expect(viewPlane.getHres()).toEqual(hres);
+        expect(viewPlane.getVres()).toEqual(vres);
+        expect(viewPlane.getPsize()).toEqual(psize);
+    })
+});
+
+
