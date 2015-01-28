a5bb56a798056b0372b1fae6a1133d40d69f55da
camargo <camargo.cac@gmail.com>
2015-01-28 10:36:38 -0800

    update SingleSphere dummy color

--- src/ts/Tracers/SingleSphere.ts
+++ src/ts/Tracers/SingleSphere.ts
@@ -1,10 +1,13 @@
 /// <reference path="Tracer.ts" />
 /// <reference path="../Utilities/Ray.ts" />
+/// <reference path="./../Utilities/RGBColor.ts" />
 
 module Tracejs {
     export class SingleSphere extends Tracer {
 
         // class methods
-        trace(ray: Ray){}
+        trace(ray: Ray) : RGBColor {
+            return new RGBColor(1.0, 0.0, 0.0); // TO DO: Implement sphere trace.
+        }
     }
 }
