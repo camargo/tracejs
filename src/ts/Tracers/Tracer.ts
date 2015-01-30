2d281e57cfef8e0d749fd3ac5ed719e763778217
Steven Esser <steven.micahel.esser@gmail.com>
2015-01-29 19:33:21 -0800

    Tracer being constructed, tests pass

--- src/ts/Tracers/Tracer.ts
+++ src/ts/Tracers/Tracer.ts
@@ -1,5 +1,25 @@
+/*
+    Steven Esser 1/29/15
+*/
+
+/* References to required definitions */
+/// <reference path="../World/ViewPlane.ts" />
+/// <reference path="../Utilities/RGBColor.ts" />
+/// <reference path="../Tracers/SingleSphere.ts" />
+/// <reference path="../Utilities/Ray.ts" />
+/// <reference path="../Utilities/Point3D.ts" />
+/// <reference path="../Utilities/Vector3D.ts" />
+/// <reference path="../World/World.ts" />
+
 module Tracejs {
     export class Tracer {
+        
+	// class properties
+	world_ptr : World;
 
+	// constructor
+	constructor(world_ptr? : World) {
+            this.world_ptr = world_ptr;
+	}
     }
 }
