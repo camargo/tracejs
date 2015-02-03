7ce4a793bcb2beb569e8b3ae3b2f32cc828386eb
Steven Esser <steven.michael.esser@gmail.com>
2015-02-03 01:13:09 -0800

    tests pass, trace seems to be working correctly, tests pass

--- src/ts/Tracers/SingleSphere.ts
+++ src/ts/Tracers/SingleSphere.ts
@@ -1,16 +1,23 @@
 // Trace.js - SingleSphere.ts
 
-/// <reference path="Tracer.ts" />
+/// <reference path="../Tracers/Tracer.ts" />
 /// <reference path="../Utilities/Ray.ts" />
 /// <reference path="./../Utilities/RGBColor.ts" />
+/// <reference path="../GeometricObjects/Primitives/Sphere.ts" />
 
 module Tracejs {
     export class SingleSphere extends Tracer {
+        
+	// properties
+        
+        // constructor
 
         // class methods
         trace(ray: Ray) : RGBColor {
+            var sphere = new Tracejs.Sphere();
+	    
 	    // need to check and see if there sphere is a hit.
-            if(true) {
+            if(sphere.hit(ray) == true) {
                 return new RGBColor(1.0, 0.0, 0.0);
 	    } else {
                 return new RGBColor(1.0, 1.0, 1.0);
--- tests/unit/Tracers/SingleSphereTest.js
+++ tests/unit/Tracers/SingleSphereTest.js
@@ -2,15 +2,34 @@
     Steven Esser 1/30/15
 */
 
-desctribe("SingleSphere class", function() {
-    it("will run trace", function() {
-       var sphere = new Tracejs.Sphere(new Tracejs.Point3D(0.0,0.0,0.0), 1.0); 
-
-       var origin = new Tracejs.Point3D(0.0,0.0,100.0);
-       var dir = new Tracejs.Vector3D(0.0,0.0,-1.0);
-
-       var ray = new Tracejs.Ray(origin, dir);
+describe("SingleSphere class", function() {
+    it("will run trace correctly when there is a hit", function() {
+        var world = new Tracejs.World();
+        expect(world).toBeDefined();
        
-       var ss = new Tracejs.SingleSphere();
+        var ss = new Tracejs.SingleSphere();
+        expect(ss).toBeDefined();
+
+        var origin = new Tracejs.Point3D(0.0, 0.0, 100.0);
+        var dir = new Tracejs.Vector3D(0.0, 0.0, -1.0);
+
+        var ray = new Tracejs.Ray(origin, dir);
+       
+        // hit will be true
+        expect(ss.trace(ray)).toEqual(new Tracejs.RGBColor(1.0, 0.0, 0.0));
+    });
+
+    it("will run trace correctly when there is not a hit", function() {
+        var world = new Tracejs.World();
+
+	var ss = new Tracejs.SingleSphere();
+
+        var origin = new Tracejs.Point3D(0.0, 2.0, 100.0);
+        var dir = new Tracejs.Vector3D(0.0, 0.0, -1.0);
+
+        var ray = new Tracejs.Ray(origin, dir);
+
+        // hit will be false (returning a black RBGColor)
+	expect(ss.trace(ray)).toEqual(new Tracejs.RGBColor(1.0, 1.0, 1.0));
     });
 });
