797b418f60aefdf99af0a2897f88816797d1eee6
kbarsalo <kbarsalo@ucsc.edu>
2015-02-07 17:08:31 -0800

    Material.ts template

---
+++ src/ts/Utilities/Material.ts
@@ -0,0 +1,20 @@
+/// <reference path="../Utilities/RGBColor.ts" />
+/// <reference path="../Utilities/ShadeRec.ts" />
+
+module Tracejs {
+    export class Material {
+
+
+    //Functions
+       shade (sr : ShadeRec) : RGBColor{
+           //TODO
+       }
+       area_light_shade(sr : ShadeRec) : RGBColor{
+           //TODO
+       }
+       path_shade(sr : ShadeRec) : RGBColor{
+           //TODO
+       }
+
+    }
+}
