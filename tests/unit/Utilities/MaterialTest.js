72a2eb024e805b421b31991476827d610584e428
kbarsalo <kbarsalo@ucsc.edu>
2015-02-09 20:21:53 -0800

    Material Test

--- tests/unit/Utilities/MaterialTest.js
+++ tests/unit/Utilities/MaterialTest.js
@@ -1,4 +1,11 @@
 describe("Material class", function() {
     it("should return black", function() {
+    	var material = new Tracejs.Material(); 
+    	
+    	expect(material.shade()).toEqual(new Tracejs.RGBColor(0,0,0));
+    	expect(material.area_light_shade()).toEqual(new Tracejs.RGBColor(0,0,0));
+    	expect(material.path_shade()).toEqual(new Tracejs.RGBColor(0,0,0));
+    	
+    	
     });
 });
