4c551324afffde99e978884346773de75d396522
jdhaytko <jdhaytko@gmail.com>
2015-02-09 23:21:14 -0800

    Base Light class and PointLight class are working

--- src/ts/Utilities/Light.ts
+++ src/ts/Utilities/Light.ts
@@ -11,19 +11,24 @@ module Tracejs {
     export class Light {
       shadows : boolean;
       
-      constructor(s : boolean){
-        this.shadows = s;
+      constructor(shadow ?: boolean){
+        if(shadow) this.shadows = shadow;
+        else       this.shadows = false;
       }
+
+      set_shadows(shadow : boolean) : void{
+        this.shadows = shadow;
+      }      
 
       get_shadows() : boolean{
         return this.shadows;
       }
       
-      get_direction() : Vector3D{
+      get_direction(sr : ShadeRec) : Vector3D{
         return new Vector3D(0.0, 0.0, 0.0);  
       }
       
-      incident_radiance() : RGBColor{
+      incident_radiance(sr : ShadeRec) : RGBColor{
         return new RGBColor(0, 0, 0);
       }
     }
