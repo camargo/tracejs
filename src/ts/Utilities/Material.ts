dfa72d169866142342a80eacd858366700ca5496
kbarsalo <kbarsalo@ucsc.edu>
2015-02-09 19:13:02 -0800

    Returns black for all functions

--- src/ts/Utilities/Material.ts
+++ src/ts/Utilities/Material.ts
@@ -1,19 +1,23 @@
 /// <reference path="../Utilities/RGBColor.ts" />
 /// <reference path="../Utilities/ShadeRec.ts" />
 
+
+//Set RGBColor to black
+var black = new Tracejs.RGBColor(0,0,0);
+
 module Tracejs {
     export class Material {
 
 
     //Functions
        shade (sr : ShadeRec) : RGBColor{
-           //TODO
+           return black;
        }
        area_light_shade(sr : ShadeRec) : RGBColor{
-           //TODO
+           return black;
        }
        path_shade(sr : ShadeRec) : RGBColor{
-           //TODO
+           return black;
        }
 
     }
