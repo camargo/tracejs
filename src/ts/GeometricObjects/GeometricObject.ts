366efb3813b30929c6b7929ed684f83344bd414a
camargo <camargo.cac@gmail.com>
2015-02-15 17:58:25 -0800

    comment out ambient_ptr

--- src/ts/GeometricObjects/GeometricObject.ts
+++ src/ts/GeometricObjects/GeometricObject.ts
@@ -3,15 +3,21 @@
 /// <reference path="./../Utilities/RGBColor.ts" />
 /// <reference path="./../Utilities/Ray.ts" />
 /// <reference path="./../Utilities/ShadeRec.ts" />
+/// <reference path="./../Materials/Material.ts" />
 
 module Tracejs {
     export class GeometricObject {
         color : RGBColor;
+        material : Material;
 
         constructor(color : RGBColor) {
             if (color) {
                 this.color = color;
             }
+        }
+
+        set_material(material : Material) {
+            this.material = material;
         }
 
         hit(ray : Ray) : boolean {
